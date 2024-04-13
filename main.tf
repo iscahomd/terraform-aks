cb851e5d2eaf6eedf2573ff81125d4d98fec5a6c
iscahomd <daniel.mohacsi.lsy@gmail.com>
2024-04-13 21:55:42 +0200

    feat: install aks with one B2s node

--- main.tf
+++ main.tf
@@ -2,3 +2,20 @@ resource "azurerm_resource_group" "this" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_kubernetes_cluster" "this" {
+  name                = var.cluster_name
+  location            = "West Europe"
+  resource_group_name = azurerm_resource_group.this.name
+  dns_prefix          = var.cluster_name
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
