1f179b6a98c5343e890fc23df528cd89e5c76966
iscahomd <daniel.mohacsi.lsy@gmail.com>
2024-04-14 10:11:04 +0200

    feat: upgrade to MySQL flexible server

--- main.tf
+++ main.tf
@@ -80,23 +80,18 @@ resource "random_password" "mysql_password" {
   upper            = true
 }
 
-resource "azurerm_mysql_server" "ghost_db" {
+resource "azurerm_mysql_flexible_server" "ghost_db" {
   name                = "ghost-db"
   location            = var.location
   resource_group_name = azurerm_resource_group.this.name
 
-  administrator_login          = "admin"
-  administrator_login_password = random_password.mysql_password.result
+  administrator_login    = "admin"
+  administrator_password = random_password.mysql_password.result
 
-  version    = "8.0"
-  storage_mb = 5120
-  sku_name   = "B_Gen4_1"
+  version  = "8.0"
+  sku_name = "B_Standard_B1s"
 
-  ssl_enforcement_enabled = false
-
-  auto_grow_enabled                 = false
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
+  backup_retention_days         = 7
+  geo_redundant_backup_enabled  = false
+  public_network_access_enabled = true
 }
