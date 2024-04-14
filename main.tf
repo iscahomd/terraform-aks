7cfdc3d5896e92c136e528d1d4aff0ce53b46b7e
iscahomd <daniel.mohacsi.lsy@gmail.com>
2024-04-14 11:37:50 +0200

    fix: allow access to mysql from azure

--- main.tf
+++ main.tf
@@ -102,3 +102,11 @@ resource "azurerm_mysql_flexible_database" "dev" {
   charset             = "utf8mb4"
   collation           = "utf8mb4_unicode_ci"
 }
+
+resource "azurerm_mysql_flexible_server_firewall_rule" "azure" {
+  name                = "single-k8s"
+  resource_group_name = azurerm_resource_group.this.name
+  server_name         = azurerm_mysql_flexible_server.ghost_db.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
